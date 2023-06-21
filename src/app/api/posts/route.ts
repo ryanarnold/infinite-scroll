c8b1fe0fa177f9f75a7a078241a2d784c883a813
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-06-21 17:45:23 +0800

    convert to infinite scroll with useInfiniteQuery

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,8 +1,13 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const posts = await prisma.post.findMany();
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const skip = parseInt(url.searchParams.get('page') as string);
+  const posts = await prisma.post.findMany({
+    take: 10,
+    skip: (skip - 1) * 10,
+  });
   return NextResponse.json(posts);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,63 @@
 'use client';
 
 import { Post } from '@prisma/client';
-import { useQuery } from '@tanstack/react-query';
+import { useIntersection } from '@mantine/hooks';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const { data } = useQuery({
-    queryKey: ['postsQuery'],
-    queryFn: async () => {
-      const { data } = await axios.get('/api/posts');
+  const { ref, entry } = useIntersection({
+    root: null,
+    threshold: 1,
+  });
+
+  const { data, fetchNextPage } = useInfiniteQuery({
+    queryKey: ['posts-query'],
+    queryFn: async ({ pageParam = 1 }) => {
+      const { data } = await axios.get(`/api/posts?page=${pageParam}`);
       return data as Post[];
     },
+    getNextPageParam: (lastPage, allPages) => {
+      return allPages.length + 1;
+    },
   });
+
+  const posts = data?.pages.flatMap((page) => page);
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+      console.log(entry.target.innerHTML);
+    }
+  }, [entry, fetchNextPage]);
 
   return (
     <main className="min-h-screen w-1/3 mt-5">
       <h1 className="text-3xl font-bold tracking-tigh mb-5">Infinite Scrolling App</h1>
       <ul className="flex flex-col gap-3">
-        {data?.map((post) => (
-          <li
-            key={post.id}
-            className="transition text-lg border border-zinc-200 rounded-md p-5 bg-zinc-50 hover:bg-zinc-100 duration-300 cursor-pointer"
-          >
-            {post.content}
-          </li>
-        ))}
+        {posts?.map((post, index) => {
+          if (index === posts.length - 5) {
+            return (
+              <li
+                key={post.id}
+                className="transition text-lg border border-zinc-200 rounded-md px-5 py-20 bg-zinc-50 hover:bg-zinc-100 duration-300 cursor-pointer"
+                ref={ref}
+              >
+                {post.content}
+              </li>
+            );
+          } else {
+            return (
+              <li
+                key={post.id}
+                className="transition text-lg border border-zinc-200 rounded-md px-5 py-20 bg-zinc-50 hover:bg-zinc-100 duration-300 cursor-pointer"
+              >
+                {post.content}
+              </li>
+            );
+          }
+        })}
       </ul>
     </main>
   );
